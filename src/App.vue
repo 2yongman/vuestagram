f3b4f86f54cd009e6c6665f6125412951abff67d
이용만 <dydaks7878@gmail.com>
2024-01-17 04:17:22 +0900

    글 발행 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,174 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li @click="step--">Cancle</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step == 1" @click="step++">Next</li>
+      <li v-if="step == 2" @click="publish">발행</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container
+    @updateStep="
+      {
+        step = $event;
+      }
+    "
+    @writeContent="
+      {
+        입력한글 = $event;
+      }
+    "
+    :data="data"
+    :step="step"
+    :imageUrl="imageUrl"
+  />
+  <button @click="more">더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input
+        @change="upload"
+        multiple
+        type="file"
+        id="file"
+        class="inputfile"
+      />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/Container.vue";
+import data from "./data.js";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Container: Container,
+  },
+  data() {
+    return {
+      data: data,
+      더보기: 0,
+      step: 0,
+      imageUrl: "",
+      입력한글: "",
+    };
+  },
+  methods: {
+    more() {
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
+        .then((result) => {
+          this.data.push(result.data);
+          this.더보기++;
+        });
+    },
+    upload(e) {
+      let file = e.target.files;
+      let url = URL.createObjectURL(file[0]); // 이미지의 임시 url, 브라우저 끄면 사라짐
+      // let replaceUrl = url.replace("blob:", ""); <- blob 삭제해줄 필요 없음
+      // console.log(replaceUrl);
+      this.step = 1;
+      this.imageUrl = url;
+    },
+    publish() {
+      console.log(this.내게시물);
+      var 내게시물 = {
+        name: "Kim Hyun",
+        userImage: "https://picsum.photos/100?random=3",
+        postImage: this.imageUrl,
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: this.입력한글,
+        filter: "perpetua",
+      };
+      this.data.unshift(내게시물); //unshift 배열 맨 앞에 넣어줌
+      this.step = 0;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+
+.inputfile {
+  display: none;
+}
+
+.input-plus {
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div
+        class="profile"
+        :style="{ backgroundImage: `url(${인스타객체.userImage})` }"
+      ></div>
+      <span class="profile-name">{{ 인스타객체.name }}</span>
+    </div>
+
+    <div
+      class="post-body"
+      :style="{ backgroundImage: `url(${인스타객체.postImage})` }"
+    ></div>
+
+    <div class="post-content">
+      <p>좋아요 수 : {{ 인스타객체.likes }}</p>
+      <p>
+        <strong>{{ 인스타객체.name }}</strong
+        >{{ 인스타객체.content }}
+      </p>
+      <p class="date">{{ 인스타객체.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    인스타객체: Object,
+  },
+};
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+
+.profile {
+  /* background-image: url("https://picsum.photos/100?random=0"); */
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+
+.post-body {
+  /* background-image: url("https://picsum.photos/600?random=0"); */
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  text-align: center;
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+
+.date {
+  font-size: 11px;
+  color: gray;
+  margin-top: -8px;
+}
+</style>
